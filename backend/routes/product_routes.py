2235a0a7366b64ac8d6e11cf1c5b53a49c827fc0
Tirth Patel <pateltirth1025@gmail.com>
2026-03-14 12:04:30 +0530

    Add SKU and product name serch name api

--- backend/routes/product_routes.py
+++ backend/routes/product_routes.py
@@ -5,8 +5,14 @@ from schemas.product_schema import ProductCreate
 from services.product_service import create_product
 from database.session import get_db
 
+from services.product_service import search_products
+
 router = APIRouter(prefix="/api/v1/products", tags=["Products"])
 
 @router.post("/")
 def add_product(product: ProductCreate, db: Session = Depends(get_db)):
     return create_product(db, product)
+
+@router.get("/search")
+def search(keyword: str, db: Session = Depends(get_db)):
+    return search_products(db, keyword)
--- backend/services/product_service.py
+++ backend/services/product_service.py
@@ -6,3 +6,23 @@ def create_product(db, product_data):
     db.commit()
     db.refresh(product)
     return product
+def search_products(db, keyword: str):
+
+    products = db.query(Product).filter(
+        Product.sku.ilike(f"%{keyword}%") |
+        Product.name.ilike(f"%{keyword}%")
+    ).all()
+
+    results = []
+
+    for p in products:
+        results.append({
+            "id": p.id,
+            "name": p.name,
+            "sku": p.sku,
+            "category": p.category,
+            "unit": p.unit,
+            "reorder_level": p.reorder_level
+        })
+
+    return results
