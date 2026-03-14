d3b0bda673a16c062d7f40b828c82c6c0e7ec3f9
Tirth Patel <pateltirth1025@gmail.com>
2026-03-14 14:53:02 +0530

    Dashboard add

--- backend/routes/profile_routes.py
+++ backend/routes/profile_routes.py
@@ -1,13 +1,26 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Header
 from sqlalchemy.orm import Session
+from jose import jwt
 
 from database.session import get_db
 from services.profile_service import get_profile
+
+SECRET_KEY = "inventory-secret"
+ALGORITHM = "HS256"
 
 router = APIRouter(prefix="/api/v1/profile", tags=["Profile"])
 
 
 @router.get("/")
-def profile(db: Session = Depends(get_db)):
-    # for hackathon we return first user
-    return get_profile(db, 1)
+def profile(
+    authorization: str = Header(None),
+    db: Session = Depends(get_db)
+):
+
+    token = authorization.split(" ")[1]
+
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+    user_id = payload["user_id"]
+
+    return get_profile(db, user_id)
